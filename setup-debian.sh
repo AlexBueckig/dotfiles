213159a00628846b3dd8603338f25b0f84cbd176
Alexander Bückig <AlexBueckig@gmail.com>
2025-02-16 15:18:55 +0100

    add ohmyposh installation

--- setup-debian.sh
+++ setup-debian.sh
@@ -9,51 +9,60 @@ requirements=("ripgrep" "bat" "stow" "curl" "git" "tmux")
 
 sudo apt update
 for requirement in "${requirements[@]}"; do
-    if ! dpkg -s "$requirement" &>/dev/null; then
-        read -p "$requirement is not installed. Install? (y/n) " -n 1 -r
-        if [[ $REPLY =~ ^[Yy]$ ]]; then
-            sudo apt install $requirement
-        fi
+  if ! dpkg -s "$requirement" &>/dev/null; then
+    read -p "$requirement is not installed. Install? (y/n) " -n 1 -r
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+      sudo apt install $requirement
     fi
+  fi
 done
+
+# Install ohmyposh
+if [ ! -d "$HOME/.local/bin/oh-my-posh" ]; then
+  read -p "ohmyposh does not exist. Install? (y/n) " -n 1 -r
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    curl -s https://ohmyposh.dev/install.sh | bash -s
+  fi
+fi
 
 # Install tpm
 if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
-    read -p "tpm does not exist. Install? (y/n) " -n 1 -r
-    echo
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
-        git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-    fi
+  read -p "tpm does not exist. Install? (y/n) " -n 1 -r
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+  fi
 fi
 
 # Install zoxide
 if ! command -v "zoxide"; then
-    read -p "zoxide does not exist. Install? (y/n) " -n 1 -r
-    echo
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
-        curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
-    fi
+  read -p "zoxide does not exist. Install? (y/n) " -n 1 -r
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
+  fi
 fi
 
 # Install fzf
 if ! command -v "fzf"; then
-    read -p "fzf does not exist. Install? (y/n) " -n 1 -r
-    echo
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
-        curl -o- -L "https://github.com/junegunn/fzf/releases/download/v$fzf_version/fzf-$fzf_version-linux_amd64.tar.gz" | sudo tar -xz -C /usr/local/bin/
-        sudo chmod +x /usr/local/bin/fzf
-    fi
+  read -p "fzf does not exist. Install? (y/n) " -n 1 -r
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    curl -o- -L "https://github.com/junegunn/fzf/releases/download/v$fzf_version/fzf-$fzf_version-linux_amd64.tar.gz" | sudo tar -xz -C /usr/local/bin/
+    sudo chmod +x /usr/local/bin/fzf
+  fi
 fi
 
 # Install kanata
 if ! command -v "kanata"; then
-    read -p "kanata does not exist. Install? (y/n) " -n 1 -r
-    echo
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
-        curl -o- -L "https://github.com/jtroo/kanata/releases/download/v$kanata_version/kanata" | sudo tar -xz -C /usr/local/bin/
-        sudo chmod +x /usr/local/bin/kanata
-        # add systemd
-    fi
+  read -p "kanata does not exist. Install? (y/n) " -n 1 -r
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    curl -o- -L "https://github.com/jtroo/kanata/releases/download/v$kanata_version/kanata" | sudo tar -xz -C /usr/local/bin/
+    sudo chmod +x /usr/local/bin/kanata
+    # add systemd
+  fi
 fi
 
 # Use stow to create symlinks
