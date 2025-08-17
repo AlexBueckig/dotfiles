504f098f0c7ed395c6fe54654e379e5071c24d30
Alexander Bückig <alex@fedora.fritz.box>
2025-08-17 20:14:16 +0200

    rewrite neovim config from scratch

--- dot-config/nvim/init.lua
+++ dot-config/nvim/init.lua
@@ -1,2 +1,6 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.globals")
+require("config.options")
+require("config.keymap")
+require("config.autocmd")
 require("config.lazy")
+require("config.colorscheme")
---
+++ dot-config/nvim/lua/config/globals.lua
@@ -0,0 +1,15 @@
+-- globals
+--------------------------------------------------------------------------------
+-- Set <space> as leader key
+-- NOTE: Must happen before loading plugins.
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true
+
+-- disable some default providers
+vim.g.loaded_node_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
