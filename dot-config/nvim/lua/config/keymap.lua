525ae8d12e09553896b843786c6ac9f98117847e
Alexander Bückig <alex@fedora.fritz.box>
2025-11-28 10:20:44 +0100

    add write command for W

--- dot-config/nvim/lua/config/keymap.lua
+++ dot-config/nvim/lua/config/keymap.lua
@@ -112,3 +112,5 @@ map("o", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next Search Result
 map("n", "N", "'nN'[v:searchforward].'zv'", { expr = true, desc = "Prev Search Result" })
 map("x", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev Search Result" })
 map("o", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev Search Result" })
+
+vim.api.nvim_create_user_command("W", "write", {})
