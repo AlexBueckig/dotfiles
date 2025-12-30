83a54ce22eeb1c28b0cade2c26502c56140aef85
Alexander Bückig <alexbueckig@gmail.com>
2025-12-30 11:47:15 +0100

    update nvim config

--- dot-config/nvim/lua/plugins/tailwind-fold.lua
+++ dot-config/nvim/lua/plugins/tailwind-fold.lua
@@ -1,6 +1,8 @@
 return {
-  'razak17/tailwind-fold.nvim',
-  opts= {},
-  dependencies = { 'nvim-treesitter/nvim-treesitter' },
-  ft = { 'html', 'svelte', 'astro', 'vue', 'typescriptreact', 'php', 'blade' },
+	"razak17/tailwind-fold.nvim",
+	opts = {
+		enabled = false,
+		ft = { "html", "svelte", "astro", "vue", "typescriptreact", "javascriptreact", "php", "blade", "htmldjango" },
+	},
+	dependencies = { "nvim-treesitter/nvim-treesitter" },
 }
