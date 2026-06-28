11945959b6cbd02b3e1af61a2c3844ee1b9db559
Alexander Bückig <alexbueckig@gmail.com>
2026-06-28 12:24:32 +0200

    update keymaps

--- dot-config/nvim/lua/plugins/todo-comments.lua
+++ dot-config/nvim/lua/plugins/todo-comments.lua
@@ -3,5 +3,10 @@ return {
 	"folke/todo-comments.nvim",
 	event = "VimEnter",
 	dependencies = { "nvim-lua/plenary.nvim" },
+    -- stylua: ignore
+    keys = {
+      { "<leader>st", function() Snacks.picker.todo_comments() end, desc = "Todo" },
+      { "<leader>sT", function () Snacks.picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } }) end, desc = "Todo/Fix/Fixme" },
+    },
 	opts = { signs = false },
 }
